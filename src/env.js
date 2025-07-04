b03cf2d890050a1732c0dcb993dbf3d3ffcaccca
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-07-04 15:29:54 +0100

    feat: added authentication using clerk

--- src/env.js
+++ src/env.js
@@ -4,13 +4,23 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    CONVEX_DEPLOYMENT: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1)
   },
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_FRONTEND_API_URL: z.string().min(1),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1)
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_CLERK_FRONTEND_API_URL: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
