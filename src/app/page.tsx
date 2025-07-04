b5e8f3379a33065264da3892a6ca587230491ca7
Raheem Orekoya <tundeorekoya1@gmail.com>
2025-07-04 20:05:53 +0100

    feat: added clerk signup webhook to create a user in db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client"
 
-import { Authenticated, Unauthenticated } from "convex/react";
+import { Authenticated, Unauthenticated, useConvexAuth } from "convex/react";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const session = useConvexAuth()
+  
   return (
     <main className="p-4">
       <Authenticated>
         <UserButton />
       </Authenticated>
       <Unauthenticated>
-        <SignInButton />
+        <SignInButton forceRedirectUrl="/dashboard"/>
       </Unauthenticated>
     </main>
   )
