bcc76ebf110531a46613a9f35781c37a3f5aa9ca
Redoxx <112984523+R4heem-Orekoya@users.noreply.github.com>
2025-07-05 18:16:28 +0100

    ui: updated dashboard sidebar icons to a new set (#4)

--- src/components/sidebar-header.tsx
+++ src/components/sidebar-header.tsx
@@ -1,14 +1,14 @@
 "use client"
 
 import Image from "next/image"
-import { SidebarHeader, SidebarMenuButton, useSidebar } from "./ui/sidebar"
+import { SidebarHeader, useSidebar } from "./ui/sidebar"
 import AppLogo from "~/public/logo.svg"
 
 export default function DashboardSidebarHeader() {
    const { open } = useSidebar()
    return (
-      <SidebarHeader data-open={open} className="flex flex-row items-center md:[&[data-open=false]]:justify-center gap-2 p-0 py-4 md:py-1 px-4 md:px-2">
-         <div data-open={open} className="relative size-8 md:[&[data-open=false]]:size-full">
+      <SidebarHeader data-open={open} className="flex flex-row items-center justify-start md:[&[data-open=false]]:justify-center transition-all duration-300 gap-2 p-0 py-4 md:py-1 px-4 md:px-2 overflow-hidden">
+         <div data-open={open} className="relative size-8 bgred">
             <Image
                src={AppLogo}
                alt="app logo"
